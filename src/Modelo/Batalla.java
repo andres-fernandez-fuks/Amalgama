c3ef450c1745bdce065394b38b1d9969c9582d87
Andres <andyfer91@gmail.com>
2021-10-17 13:37:41 -0300

    Las batallas devuelven el id del ejercito ganador

--- src/Modelo/Batalla.java
+++ src/Modelo/Batalla.java
@@ -3,15 +3,19 @@ package Modelo;
 import Modelo.Ejercito.Ejercito;
 
 public class Batalla {
+    private final int ganador;
     public Batalla(Ejercito ejercito1, Ejercito ejercito2) {
         int fuerza_ejercito1 = ejercito1.fuerza();
         int fuerza_ejercito2 = ejercito2.fuerza();
         if (fuerza_ejercito1 > fuerza_ejercito2) {
             resolverGanadorPerdedor(ejercito1, ejercito2);
+            ganador = ejercito1.getId();
         } else if (fuerza_ejercito1 == fuerza_ejercito2) {
             resolverEmpate(ejercito1, ejercito2);
+            ganador = 0;
         } else {
             resolverGanadorPerdedor(ejercito2, ejercito1);
+            ganador = ejercito2.getId();
         }
     }
 
@@ -24,4 +28,6 @@ public class Batalla {
         ejercito1.afrontarEmpate();
         ejercito2.afrontarEmpate();
     }
+
+    public int ganador() { return ganador; }
 }
--- test/BatallaTest.java
+++ test/BatallaTest.java
@@ -10,14 +10,16 @@ public class BatallaTest {
     @Test
     public void siUnEjercitoPierdeUnaBatallaDeberiaPerderSusDosUnidadesDeMayorPuntaje() {
         int cantidad_de_caballeros_inicial = ejercito_debil.cantidadDeCaballeros();
-        new Batalla(ejercito_debil, ejercito_fuerte);
+        Batalla batalla = new Batalla(ejercito_debil, ejercito_fuerte);
+        assert(batalla.ganador() == ejercito_fuerte.getId());
         assert(ejercito_debil.cantidadDeCaballeros() == cantidad_de_caballeros_inicial -2);
     }
 
     @Test
     public void siUnEjercitoGanaUnaBatallaDeberiaPObtener100UnidadesExtraDeOro() {
         int cantidad_inicial_de_oro = ejercito_fuerte.oroDisponible();
-        new Batalla(ejercito_debil, ejercito_fuerte);
+        Batalla batalla = new Batalla(ejercito_debil, ejercito_fuerte);
+        assert(batalla.ganador() == ejercito_fuerte.getId());
         assert(ejercito_fuerte.oroDisponible() == cantidad_inicial_de_oro + 100);
     }
 
@@ -25,7 +27,8 @@ public class BatallaTest {
     public void siDosEjercitosEmpatanUnaBatallaAmbosDeberianPerderSuUnidadDeMayorPuntaje() {
         int cantidad_de_caballeros_inicial_1 = ejercito_fuerte.cantidadDeCaballeros();
         int cantidad_de_caballeros_inicial_2 = ejercito_igualado.cantidadDeCaballeros();
-        new Batalla(ejercito_fuerte, ejercito_igualado);
+        Batalla batalla = new Batalla(ejercito_fuerte, ejercito_igualado);
+        assert(batalla.ganador() == 0);
         assert(ejercito_fuerte.cantidadDeCaballeros() == cantidad_de_caballeros_inicial_1 -1);
         assert(ejercito_igualado.cantidadDeCaballeros() == cantidad_de_caballeros_inicial_2 -1);
     }
